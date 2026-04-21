cd9357bd7a1dde8547c192a35d665f496bae13cd
Amantay Bagdat <amantabagdat9@gmail.com>
2026-04-21 11:13:42 +0500

    Add Django backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -68,12 +68,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default='kbtu_platform'),
-        'USER': config('DB_USER', default='postgres'),
-        'PASSWORD': config('DB_PASSWORD', default='postgres'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
